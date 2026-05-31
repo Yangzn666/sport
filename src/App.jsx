0b57cc082f43c2c2842d75eebf6de526da05c349
杨臻宁 <2142744149@qq.com>
2026-05-31 17:59:54 +0800

    Fix GitHub Pages routing: change BrowserRouter to HashRouter for static hosting compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import KnowledgeBase from './pages/KnowledgeBase';
 import Category from './pages/Category';
